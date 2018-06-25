463435094f163f33bdf1c589fa2574538fcc04c1
Chris Black <38511950+CBlack10@users.noreply.github.com>
2018-06-25 13:20:40 -0500

    adding finishing touches and removing console.logs

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-
+//Global Variables to be used later
     var ranNum;
     var wins = 0;
     var losses = 0;
@@ -30,10 +30,10 @@ $(document).ready(function () {
     }
     //pushing variable to global var
     ranNum = generateNum();
-    console.log(ranNum);
+
 
     //writing to screen
-    $("#guess-card").text(ranNum);
+    $("#guess-card").text("Number To Match: " + ranNum);
 
     //gems random number function
     function generateGemNum() {
@@ -41,7 +41,6 @@ $(document).ready(function () {
     }
     //assiging to global var
     gemNum = generateGemNum();
-    console.log(gemNum);
 
     function checkGem(id) {
         for (i = 0; i < gemArray.length; i++) {
@@ -51,19 +50,22 @@ $(document).ready(function () {
         }
     }
 
+    //function to reset the game if won/loss
     function resetGame() {
         ranNum = generateNum();
         for (i = 0; i < gemArray.length; i++) {
             gemArray[i].randomNum = 0;
         }
         currentScore = 0;
-        $("#guess-card").text(ranNum);
+        $("#guess-card").text("Number to Match: " + ranNum);
     }
-    function handleScore(){
+    //score handler
+    function handleScore() {
         $("#winner").text("Wins: " + wins);
         $("#loser").text("Losses: " + losses)
     }
 
+    //sorts the crystals random numbers a stores them to global variables until reset
     $(".gem").on("click", function () {
         if (checkGem(this.id)) {
             for (j = 0; j < gemArray.length; j++) {
@@ -72,23 +74,22 @@ $(document).ready(function () {
                     currentScore = currentScore + gemArray[j].randomNum;
                 } else if (gemArray[j].id === this.id) {
                     currentScore = currentScore + gemArray[j].randomNum;
-                    console.log(currentScore)
                 }
             }
 
         }
-    
 
+        //conditionals for wins/losses to reset game
         if (currentScore === ranNum) {
             wins++;
             resetGame();
             handleScore();
 
-        } else if(currentScore > ranNum) {
+        } else if (currentScore > ranNum) {
             losses++;
             resetGame();
             handleScore();
-        } 
+        }
         $("#final-score").text(currentScore);
     });
 
